eb1e9d59f2caaca3e8792d945274042773b86414
ryg <ryg@banncity.de>
2014-09-20 16:18:52 +0200

    frmTagVerifyer: Anzeige der Albumdaten

--- iTagger/frmTagVerifyer.vb
+++ iTagger/frmTagVerifyer.vb
@@ -5,8 +5,14 @@
     End Sub
 
     Public Sub FillData(albumId As Integer)
+        'Laden der API Daten
         Dim r As str_search_result = api_lookup(albumId)
 
+        'Anzaigen der Albumdaten
         LoadWebImageToPictureBox(picCover, r.tracks(0).artworkUrl600)
+        txtAlbumName.Text = r.tracks(0).collectionCensoredName
+        txtArtist.Text = r.tracks(0).artistiName
+        txtGenre.Text = r.tracks(0).primaryGenreName
+        txtYear.Text = r.tracks(0).releaseDate
     End Sub
 End Class
